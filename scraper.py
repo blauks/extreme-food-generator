a4a67b2588745f4f816ba9d67352ba6a92e1af95
Jonathan Brooks <brooks.jonathan1@gmail.com>
2022-05-10 20:23:16 +0200

    Make scraper better, and put entire matprat into JSON file:D

--- scraper.py
+++ scraper.py
@@ -6,6 +6,7 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
+import requests
 
 import json
 
@@ -50,14 +51,17 @@ def formatIngredient(raw):
     return f"{amount}{unit} {ingredient}"
 
 def extractRecipe(recipeURL):
-    driver.get("https://www.matprat.no"+recipeURL)
-    WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.CLASS_NAME, "recipe-search-tags__item"))
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    source = requests.get("https://www.matprat.no"+recipeURL).content
+    soup = BeautifulSoup(source, 'html.parser')
     attributes = [att.get_text() for att in soup.find_all(attrs={"class": "recipe-search-tags__item"})]
     title = soup.find(attrs={"class": "article-title lp_is_start"}).get_text()
     description = soup.find(attrs={"itemprop": "description"}).find("p").get_text()
     steps = formatSteps(soup.find(attrs={"class": "recipe-steps"}).find_all(attrs={"class": "recipe-steps__item"}))
-    numberOfPeople = soup.find(attrs={"id": "portionsInput"})["value"]
+    numberOfPeople = ""
+    try:
+        numberOfPeople = soup.find(attrs={"id": "portionsInput"})["value"]
+    except:
+        numberOfPeople = soup.find(attrs={"class": "portions-label"}).get_text().strip()
     # Matprat skal selvfølgelig drive å ha både mobil og pc HTML i samme fil, bare i tilfelle noen plutselig bytter over til mobilen deres mens de ser på oppskrifter på pcen :|
     ingredients = [formatIngredient(item) for item in soup.find(attrs={"class": "grid__item cm-module--white one-whole recipe-ingredients"}).find_all(attrs={"itemprop":"ingredients"})]
     time = soup.find(attrs={"data-epi-property-name": "RecipeTime"}).get_text()
@@ -71,9 +75,8 @@ def findRecipes(pageChildren):
         recipeObject = extractRecipe(recipeURL)
         if not recipeObject == None: recipes.append(recipeObject)
 
-def findRecipeURLs(s):
-    recipeElements = soup.find(attrs={"id": "recipeListResults"}).children
-    urls = [child.find("a")['href'] for child in recipeElements]
+def findRecipeURLs(hits):
+    urls = [child["linkUrl"] for child in hits]
     return urls
 
 recipeUrls = []
@@ -81,21 +84,21 @@ recipeUrls = []
 for p in range(1, maxPage+1):
     print(f"Page {p} of {maxPage} starting...")
     print(url+str(p))
-    driver.get(url+str(p))
-    driver.refresh()
-    WebDriverWait(driver, timeout=3).until(lambda d: d.find_element(By.ID, "recipeListResults"))
+    #source = requests.get(url+str(p), cookies={"consent-set": "true", "marketing-cookies-consent": "granted", "statistics-cookies-consent": "granted", "ARRAffinity": "95e5f71f2cafafb58c0c651bc100e82035e010c66b371ba830f2463be02741ec", ".ASPXANONYMOUS": "Xy_mAmQJR9XyKdLKyD2qCkTyQlWzLw-6OjguJDZFTiww8fmLUkM0GfhQxRVDqwLfKxRJ8JpCgN4o57THNTEV1EFEEjKbm1Edb9CJP1khakEmGM8KUQhZ6mvut6zVs_G_gGLYbA2", "_ab_test": "Q+3o5ipwUEdYF2edMl0Uk3jZwd3ux3rUAFaUuWChVHvfqq7cYRcx2AE218zHUqO0xPk1wg=="}).content
+    data = json.loads(requests.get("https://www.matprat.no/api/RecipeSearch/GetRecipes?text=&page=1&sort=new", headers={'referer': url+str(p)}).content)
+    recipeUrls += findRecipeURLs(data["searchHits"])
     print(len(recipeUrls))
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
-    recipeUrls += findRecipeURLs(soup)
     print(f"...Page {p} of {maxPage} done, continuing")
-    # Max for now
-    if len(recipeUrls) > 200:
-        break
 
 
 for i, url in enumerate(recipeUrls):
-    print(url)
-    recipeObject = extractRecipe(url)
-    if not recipeObject == None: recipes.append(recipeObject) 
+    try:
+        recipeObject = extractRecipe(url)
+        print(f"{i} of {len(recipeUrls)}: ",url)
+        if not recipeObject == None: recipes.append(recipeObject) 
+    except:
+        print("DIDN'T WORK, PAUSE")
+        sleep(3)
+        pass
 
 open("recipesTEST.json", "w").write(json.dumps(recipes, indent=4, ensure_ascii=False))
