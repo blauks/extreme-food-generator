f7a331fe162d569ff6367fa2485e47bfd4c4f28d
Jonathan Brooks <brooks.jonathan1@gmail.com>
2022-05-13 16:51:57 +0200

    Final delivery maybe

--- recipeGenerator.py
+++ recipeGenerator.py
@@ -1,31 +1,24 @@
+# SETUP
 import json
-from pyrsistent import v
 import tensorflow as tf
 import matplotlib.pyplot as plt
 import numpy as np
 import os
 
-#
-# WILL PROBABLY NEED MORE DATA
-#
 file = open("recipes.json")
 dataDict = json.loads(file.read())
 file.close()
 
-#
-# MIGHT ADD FILTERING AT A LATER STAGE
-# 
-
-
 # STOPWORDS
-STOP_WORD_TITLE="📖 "
-STOP_WORD_ATTRIBUTES="\n\n📝\n"
-STOP_WORD_DESCRIPTION="\n\n🎧\n"
-STOP_WORD_NUMBER_OF_PEOPLE="\n\n👨‍🔧\n"
-STOP_WORD_TIME="\n\n⏰\n"
-STOP_WORD_DIFFICULTY="\n\n😓\n"
-STOP_WORD_INGREDIENTS="\n\n🥗\n"
-STOP_WORD_STEPS="\n\n👨‍🍳\n"
+STOP_WORD_TITLE="[TITTEL]\n"
+STOP_WORD_ATTRIBUTES="\n\n[ATTRIBUTTER]\n"
+STOP_WORD_DESCRIPTION="\n\n[INTRODUKSJON]\n"
+STOP_WORD_NUMBER_OF_PEOPLE="\n\n[ANTALL PORSJONER]\n"
+STOP_WORD_TIME="\n\n[TID]\n"
+STOP_WORD_DIFFICULTY="\n\n[VANSKLIGHETSGRAD]\n"
+STOP_WORD_INGREDIENTS="\n\n[INGREDIENSER]\n* "
+STOP_WORD_STEPS="\n\n[STEG]\n- "
+
 
 # CONVERT RECIPE OBJECT TO STRING
 def recipeToString(recipe):
@@ -39,26 +32,35 @@ def recipeToString(recipe):
     steps = recipe["steps"]
 
     stringyfiedRecipe = STOP_WORD_TITLE + title
-    stringyfiedRecipe += STOP_WORD_ATTRIBUTES + attributes
+    stringyfiedRecipe += STOP_WORD_ATTRIBUTES + " - ".join(attributes)
     stringyfiedRecipe += STOP_WORD_DESCRIPTION + description
-    stringyfiedRecipe += STOP_WORD_NUMBER_OF_PEOPLE + nPeople + " personer"
+    stringyfiedRecipe += STOP_WORD_NUMBER_OF_PEOPLE + nPeople
     stringyfiedRecipe += STOP_WORD_TIME + time
     stringyfiedRecipe += STOP_WORD_DIFFICULTY + difficulty
-    stringyfiedRecipe += STOP_WORD_INGREDIENTS + "\n".join(ingredients)
-    stringyfiedRecipe += STOP_WORD_STEPS + "\n".join(steps)
+    stringyfiedRecipe += STOP_WORD_INGREDIENTS + "\n* ".join(ingredients)
+    stringyfiedRecipe += STOP_WORD_STEPS + "\n- ".join(steps)
 
     return stringyfiedRecipe
 
-stringyfiedData = [recipeToString(recipe) for recipe in dataDict]
+stringyfiedData = np.array([recipeToString(recipe) for recipe in dataDict])
 
-# recipeLengths = [len(recipe) for recipe in stringyfiedData]
-# print(f"Longest recipe: {max(recipeLengths)}")
-# 2459
-# Can get most recipes if max length is 2000
-MAX_RECIPE_LENGTH = 2000
+print(f"Number of recipes: {len(stringyfiedData)}")
+print(stringyfiedData[0])
 
+recipeLengths = [len(recipe) for recipe in stringyfiedData]
+
+plt.hist(recipeLengths, bins=50)
+plt.show()
+
+MAX_RECIPE_LENGTH = 1800
+
+# FILTER OUT TOO LARGE RECIPES
 filteredDataset = [recipe for recipe in stringyfiedData if len(recipe) <= MAX_RECIPE_LENGTH]
 
+print(f"Number of filtered recipes: {len(filteredDataset)}")
+print(f"Number of recipes removed: {len(stringyfiedData)- len(filteredDataset)}")
+
+# VECTORIZE RECIPES
 END_SIGN="~"
 
 tokenizer = tf.keras.preprocessing.text.Tokenizer(
@@ -74,6 +76,8 @@ tokenizer.fit_on_texts(filteredDataset)
 
 # + 1 since index 0 is reserved
 VOCABULARY_SIZE = len(tokenizer.word_counts) + 1 
+
+print(f"Vocabulary Size: {VOCABULARY_SIZE}")
 
 vectorizedData = tokenizer.texts_to_sequences(filteredDataset)
 
@@ -99,6 +103,7 @@ vectorizedDataPadded = tf.keras.preprocessing.sequence.pad_sequences(
     value=tokenizer.texts_to_sequences([END_SIGN])[0]
 )
 
+#CONVERT TO TENSOR FLOW DATASET, AND SPLIT INTO INPUT AND TARGET
 tfDataset = tf.data.Dataset.from_tensor_slices(vectorizedDataPadded)
 
 def splitInputAndTarget(recipe):
@@ -106,11 +111,13 @@ def splitInputAndTarget(recipe):
 
 tfDatasetTargeted = tfDataset.map(splitInputAndTarget)
 
-BATCH_SIZE=32
+#SPLIT INTO BATCHES
+BATCH_SIZE=64
 SHUFFLE_BUFFER_SIZE=1000
 
 tfDatasetTrain = tfDatasetTargeted.shuffle(SHUFFLE_BUFFER_SIZE).batch(BATCH_SIZE, drop_remainder=True).repeat()
 
+#BUILD MODEL
 def buildModel(vocabSize, embeddingDim, rnnUnits, batchSize):
     model = tf.keras.models.Sequential()
     model.add(tf.keras.layers.Embedding(
@@ -118,20 +125,22 @@ def buildModel(vocabSize, embeddingDim, rnnUnits, batchSize):
         output_dim=embeddingDim,
         batch_input_shape=[batchSize, None]
     ))
-    model.add(tf.keras.layers.LSTM(
+    model.add(tf.keras.layers.GRU(
         units=rnnUnits,
+        activation="tanh",
+        recurrent_activation="sigmoid",
         return_sequences=True,
         stateful=True,
-        recurrent_initializer=tf.keras.initializers.GlorotNormal()
     ))
     model.add(tf.keras.layers.Dense(vocabSize))
 
     return model
 
+#DEFINE LOSS AND OPTIMIZER
 model = buildModel(
     vocabSize=VOCABULARY_SIZE,
     embeddingDim=256,
-    rnnUnits=1024,
+    rnnUnits=750,
     batchSize=BATCH_SIZE
 )
 
@@ -157,6 +166,7 @@ earlyStopping = tf.keras.callbacks.EarlyStopping(
     verbose=1
 )
 
+#STORE CHECKPOINTS
 checkpointDirectory = 'tmp/checkpoints'
 os.makedirs(checkpointDirectory, exist_ok=True)
 checkpointPrefix = os.path.join(checkpointDirectory, 'ckpt_{epoch}')
@@ -165,23 +175,10 @@ checkpointCallback=tf.keras.callbacks.ModelCheckpoint(
     save_weights_only=True
 )
 
-EPOCHS = 20
-INITIAL_EPOCH = 1
+#RUN MODEL
+EPOCHS = 10
+INITIAL_EPOCH = 0
 STEPS_PER_EPOCH = 1500
-
-history = model.fit(
-    x=tfDatasetTrain,
-    epochs=EPOCHS,
-    steps_per_epoch=STEPS_PER_EPOCH,
-    initial_epoch=INITIAL_EPOCH,
-    callbacks=[
-        checkpointCallback,
-        earlyStopping
-    ]
-)
-
-modelName = 'recipeGenerationRaw.h5'
-model.save(modelName, save_format='h5')
 
 def render_training_history(training_history):
     loss = training_history.history['loss']
@@ -193,5 +190,88 @@ def render_training_history(training_history):
     plt.grid(linestyle='--', linewidth=1, alpha=0.5)
     plt.show()
 
+history = model.fit(
+    x=tfDatasetTrain,
+    epochs=EPOCHS,
+    steps_per_epoch=STEPS_PER_EPOCH,
+    initial_epoch=INITIAL_EPOCH,
+    callbacks=[
+        checkpointCallback,
+        earlyStopping
+    ]
+)
+modelName = 'recipeGenerationRaw.h5'
+model.save(modelName, save_format='h5')
 render_training_history(history)
+
+#BUILD MODEL AGAIN WITH DIFFERENT BATCH SIZE TO BE ABLE TO USE CUSTOM INPUT LENGTHS
+SIMPLIFIED_BATCH_SIZE=1
+
+simplifiedModel = buildModel(vocabSize=VOCABULARY_SIZE,
+    embeddingDim=256,
+    rnnUnits=750,
+    batchSize=SIMPLIFIED_BATCH_SIZE)
+simplifiedModel.load_weights(tf.train.latest_checkpoint(checkpointDirectory))
+simplifiedModel.build(tf.TensorShape([SIMPLIFIED_BATCH_SIZE, None]))
+
+#GENERATE RECIPE
+def generateText(model, startString, numGenerate=1000, temperature=1.0):
+  startString = STOP_WORD_TITLE + startString
+  inputIndices = np.array(tokenizer.texts_to_sequences([startString]))
+  generatedText = []
+  model.reset_states()
+  
+  for charIndex in range(numGenerate):
+    predictions = model(inputIndices)
+    predictions = tf.squeeze(predictions, 0)
+    predictions = predictions / temperature
+    predictedId = tf.random.categorical(
+            predictions,
+            num_samples=1
+        )[-1, 0].numpy()
+    inputIndices = tf.expand_dims([predictedId], 0)
+    nextCharacter = tokenizer.sequences_to_texts(inputIndices.numpy())[0]
+    if nextCharacter == "~":
+        break
+    generatedText.append(nextCharacter)
+  
+  return (startString + ''.join(generatedText))
+
+#Change temperature to make output more or less random. 1.2 seems to work well.
+print(generateText(simplifiedModel, startString="K", numGenerate=1800, temperature=1.2))
+
+
+# Perform tests on model
+import nltk
+import random
+from nltk.translate.bleu_score import SmoothingFunction, corpus_bleu
+
+def getRandomLetter():
+    return chr(random.randint(ord('A'), ord('Z')))
+
+def generateRandomRecipes(n=10):
+    recipes = []
+    for i in range(n):
+        recipes.append(generateText(simplifiedModel, startString=getRandomLetter(), numGenerate=1800, temperature=1.2))
+        print(f"{len(recipes)} out of {n} generated...")
+    return recipes
+
+referenceRecipes = filteredDataset[:10]
+generatedRecipes = generateRandomRecipes()
+
+def bleu(references, generated):
+    splitReferences = [ref.split() for ref in references]
+    splitGenerated = [gen.split() for gen in generated]
+    cc = SmoothingFunction()    
+    return corpus_bleu(splitReferences, splitGenerated, weights=(0.25, 0.25, 0.25, 0.25), smoothing_function=cc.method4)
+
+print(f"BLEU Score: {bleu(referenceRecipes, generatedRecipes)}")
+
+from rouge import Rouge
+
+def rouge(generated, references):
+    rouge = Rouge()
+    return rouge.get_scores(generated, references, avg=True)
+
+print(rouge(generatedRecipes, referenceRecipes))
 
